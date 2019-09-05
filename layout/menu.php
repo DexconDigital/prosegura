e1dce99368ba32aec160ba83f3f2088abce64ee9
william solano <wdsp9898@gmail.com>
2019-09-05 09:38:10 -0500

    Arreglos
    Sl paginador esta funcionando correctamente.

--- layout/menu.php
+++ layout/menu.php
@@ -78,7 +78,7 @@
       <li class="nav-item espacio_menu <?php if ($page == 'Inmuebles') {
                                           echo 'active';
                                         } ?> ">
-        <a class="nav-link" href="inmueble.php"> Inmuebles</a>
+        <a class="nav-link" href="inmueble.php?"> Inmuebles</a>
       </li>
       <li class="nav-item espacio_menu dropdown">
         <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
