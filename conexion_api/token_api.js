65454af3825746dcc87b2f080b00c9666a9cce4e
william solano <wdsp9898@gmail.com>
2019-09-17 09:38:32 -0500

    Se realiza configuracion de protocolo
    Se adiciona una variable de llamdo para el protocolo

--- conexion_api/token_api.js
+++ conexion_api/token_api.js
@@ -1,3 +1,7 @@
 const TOKEN = 'jhMmWQpjL3qVxTrcyV5Xs0ySGaiTrbciJNpThWHb-805';
 //rf234BcAxcy9PbCDxxLRO752bzQlU547vKES0zPF-60
     //jhMmWQpjL3qVxTrcyV5Xs0ySGaiTrbciJNpThWHb-805
+
+const PROTOCOLO = 'https';
+
+
--- variables/config.php
+++ variables/config.php
@@ -2,4 +2,6 @@
     define('TOKEN', 'jhMmWQpjL3qVxTrcyV5Xs0ySGaiTrbciJNpThWHb-805');
     //rf234BcAxcy9PbCDxxLRO752bzQlU547vKES0zPF-60
     //jhMmWQpjL3qVxTrcyV5Xs0ySGaiTrbciJNpThWHb-805
+    define('PROTOCOLO', 'https' );
+
 ?>
