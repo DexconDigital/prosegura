1148681cd8155c2d4fec7f6ccd66f81540ca1310
william solano <wdsp9898@gmail.com>
2020-01-16 11:08:43 -0500

    arreglos en detalle de inmueble

--- detalle_inmueble.php
+++ detalle_inmueble.php
@@ -268,7 +268,7 @@ $page = 'Inmuebles' ?>
                             </div>
 
                             <div class="col-12 col-md-12 col-lg-12">
-                                <div class="row">
+                                <div class="">
                                     <?php similares($r['IdCiudad'], $r['IdTpInm']); ?>
                                 </div>
                             </div>
